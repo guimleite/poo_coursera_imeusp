84a5643450af94019f600b34fd380b147476d9e0
guimleite <guilhermemleitte@hotmail.com>
2022-07-21 20:07:25 -0300

    adição arquivos feitos pelo curso de poo-ime-usp

---
+++ carregador/Carregador.py
@@ -0,0 +1,20 @@
+
+# classe para um carregador de iPhone
+
+class IPhone
+    ___name__='iPhone'
+
+    def __init__(self):
+        self.conectado = False
+
+    def usar_lightning(self):
+        self.conectado = True
+        print("Conectado ao carregador lightning")
+
+    def recarregar(self):
+        if self.conectado:
+            print("Carregando...")
+            print("Bateria recarregada!")
+        else:
+            print("Conecte ao carregador lighting antes")
+
